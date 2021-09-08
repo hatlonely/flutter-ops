7266585256e648afda8325977e9bd5692a96b85b
hatlonely.hl <hatlonely.hl@alibaba-inc.com>
2021-09-09 01:06:50 +0800

    update

--- flutter_ops/lib/pages/repository/create_repository.dart
+++ flutter_ops/lib/pages/repository/create_repository.dart
@@ -12,10 +12,72 @@ class CreateRepositoryPage extends StatelessWidget {
           child: SizedBox(
             width: 800,
             child: Column(
-              children: [Text("hello world")],
+              children: [CreateRepositoryWidget()],
             ),
           ),
         ),
+      ),
+    );
+  }
+}
+
+class CreateRepositoryWidget extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _CreateRepositoryWidgetState();
+}
+
+class _CreateRepositoryWidgetState extends State<CreateRepositoryWidget> {
+  final _formKey = GlobalKey<FormState>();
+
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _endpointController = TextEditingController();
+  final TextEditingController _nameController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(10))),
+      margin: EdgeInsets.fromLTRB(10, 50, 10, 20),
+      child: Padding(
+        padding: EdgeInsets.all(10),
+        child: Column(children: [
+          Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                    ["用户名", "请输入用户名", _usernameController],
+                    ["密码", "请输入密码", _passwordController],
+                    ["代码仓库地址", "github.com", _endpointController],
+                    ["仓库名", "hatlonely/ops.git", _nameController],
+                  ]
+                      .map(
+                        (e) => [
+                          TextFormField(
+                            controller: e[2] as TextEditingController,
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: e[0] as String,
+                              hintText: e[1] as String,
+                            ),
+                          ),
+                          SizedBox(height: 10),
+                        ],
+                      )
+                      .expand((x) => x)
+                      .toList() +
+                  [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        ElevatedButton(onPressed: () {}, child: Text("创建")),
+                      ],
+                    ),
+                  ],
+            ),
+          ),
+        ]),
       ),
     );
   }
