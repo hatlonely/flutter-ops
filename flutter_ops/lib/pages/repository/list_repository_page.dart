ab04c3a21235473d52096894fee36f2f52e1ffa8
hatlonely.hl <hatlonely.hl@alibaba-inc.com>
2021-09-12 02:20:33 +0800

    update

--- flutter_ops/lib/pages/repository/list_repository_page.dart
+++ flutter_ops/lib/pages/repository/list_repository_page.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:opsapi/opsapi.dart';
 
 class RepositoryPage extends StatelessWidget {
   @override
@@ -23,49 +24,67 @@ class RepositoryPage extends StatelessWidget {
 }
 
 class ListRepositoryWidget extends StatelessWidget {
-  // static const _pageGrid = [
-  //   ["repository0"],
-  //   ["repository1"],
-  //   ["repository2"],
-  // ];
-
-  _listRepository() async {
-    var res = await Dio().get('http://k8s.rpc.ops.hatlonely.com/v1/repository');
-    return res;
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 5,
-      shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(10))),
-      margin: EdgeInsets.fromLTRB(10, 50, 10, 20),
-      child: Padding(
-        padding: EdgeInsets.all(10),
-        child: GridView.extent(
-          primary: false,
-          padding: const EdgeInsets.all(20),
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
-          shrinkWrap: true,
-          maxCrossAxisExtent: 200.0,
-          physics: NeverScrollableScrollPhysics(),
-          children: [
-            Card(
-              elevation: 5,
-              color: Theme.of(context).colorScheme.secondary,
-              shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(10))),
-              margin: EdgeInsets.fromLTRB(10, 10, 10, 10),
-              child: InkWell(
-                child: Center(
-                  child: Icon(Icons.add, color: Colors.white),
+    return FutureBuilder<Response<ApiListRepositoryRes>>(future: () {
+      final client = Opsapi(
+        basePathOverride: 'http://k8s.rpc.ops.hatlonely.com',
+      ).getOpsServiceApi();
+      return client.opsServiceListRepository();
+    }(), builder: (BuildContext context, AsyncSnapshot<Response<ApiListRepositoryRes>> res) {
+      List<Card> cards = [];
+      if (res.hasData) {
+        cards = res.data!.data!.repositories!
+            .where((e) => e.name != null)
+            .map(
+              (e) => Card(
+                elevation: 5,
+                color: Theme.of(context).colorScheme.secondary,
+                shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(10))),
+                margin: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                child: InkWell(
+                  child: Center(
+                    child: Text(e.name!, style: TextStyle(color: Colors.white)),
+                  ),
+                  onTap: () => Navigator.pushNamed(context, '/repository/create'),
                 ),
-                onTap: () => Navigator.pushNamed(context, '/repository/create'),
               ),
             )
-          ],
+            .toList();
+      }
+
+      return Card(
+        elevation: 5,
+        shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(10))),
+        margin: EdgeInsets.fromLTRB(10, 50, 10, 20),
+        child: Padding(
+          padding: EdgeInsets.all(10),
+          child: GridView.extent(
+            primary: false,
+            padding: const EdgeInsets.all(20),
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10,
+            shrinkWrap: true,
+            maxCrossAxisExtent: 200.0,
+            physics: NeverScrollableScrollPhysics(),
+            children: cards +
+                [
+                  Card(
+                    elevation: 5,
+                    color: Theme.of(context).colorScheme.secondary,
+                    shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(10))),
+                    margin: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                    child: InkWell(
+                      child: Center(
+                        child: Icon(Icons.add, color: Colors.white),
+                      ),
+                      onTap: () => Navigator.pushNamed(context, '/repository/create'),
+                    ),
+                  ),
+                ],
+          ),
         ),
-      ),
-    );
+      );
+    });
   }
 }
